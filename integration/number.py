b4f5bc707e3daf1fd43c66e31ffaba6da248e1df
NicyJose <nicy8491@gmail.com>
2024-02-16 09:49:15 -0600

    new files are added in integration folder

---
+++ integration/number.py
@@ -0,0 +1,12 @@
+def positive(number):
+    if number > 0:
+        return "positive"
+    else:
+        return "zero"
+
+
+def negative(number):
+    if number < 0:
+        return "negative"
+    else:
+        return "zero"
---
+++ integration/positive_test.py
@@ -0,0 +1,9 @@
+from NicyJose_8942241.integration import number
+
+
+def test_positive():
+    assert number.positive(5) == "positive"
+
+
+def test_negative():
+    assert number.negative(-5) == "positive"
